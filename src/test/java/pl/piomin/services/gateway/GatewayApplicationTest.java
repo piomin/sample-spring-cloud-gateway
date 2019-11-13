77cff83fa059968c43c36c3cddaf9787a772bca8
minkowp <piotr.minkowski@play.pl>
2019-11-13 11:43:02 +0100

    remove hoverfly

--- src/test/java/pl/piomin/services/gateway/GatewayApplicationTest.java
+++ src/test/java/pl/piomin/services/gateway/GatewayApplicationTest.java
@@ -1,31 +1,24 @@
 package pl.piomin.services.gateway;
 
-import io.specto.hoverfly.junit.core.SimulationSource;
-import io.specto.hoverfly.junit.dsl.HoverflyDsl;
-import io.specto.hoverfly.junit.rule.HoverflyRule;
 import org.junit.Assert;
-import org.junit.ClassRule;
 import org.junit.Test;
-import pl.piomin.services.gateway.model.Account;
-
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-
-import static io.specto.hoverfly.junit.dsl.ResponseCreators.success;
-import static io.specto.hoverfly.junit.dsl.matchers.HoverflyMatchers.startsWith;
+import org.springframework.test.context.junit4.SpringRunner;
+import pl.piomin.services.gateway.model.Account;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@RunWith(SpringRunner.class)
 public class GatewayApplicationTest {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(GatewayApplicationTest.class);
 
 	@Autowired
 	TestRestTemplate template;
-
-	@ClassRule
-	public static HoverflyRule hoverflyRule = HoverflyRule
-			.inSimulationMode(SimulationSource.dsl(HoverflyDsl.service("localhost:8091").get(startsWith("/accounts/"))
-					.willReturn(success("[{\"id\":\"1\",\"number\":\"1234567890\"}]", "application/json"))))
-			.printSimulationData();
 
 	@Test
 	public void testAccountService() {
