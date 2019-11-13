14ec713353b3bed949f4b0a336dc71a9ffa6afb3
minkowp <piotr.minkowski@play.pl>
2019-11-13 14:46:48 +0100

    adding testcontainers with mock server

--- src/test/java/pl/piomin/services/gateway/GatewayApplicationTest.java
+++ src/test/java/pl/piomin/services/gateway/GatewayApplicationTest.java
@@ -1,15 +1,20 @@
 package pl.piomin.services.gateway;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
+import org.mockserver.client.server.MockServerClient;
+import org.mockserver.model.HttpRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.mock.web.reactive.function.server.MockServerRequest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.MockServerContainer;
 import pl.piomin.services.gateway.model.Account;
+
+import static org.mockserver.model.HttpResponse.response;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @RunWith(SpringRunner.class)
@@ -17,8 +22,25 @@ public class GatewayApplicationTest {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(GatewayApplicationTest.class);
 
+	@ClassRule
+	public static MockServerContainer mockServer = new MockServerContainer();
+
 	@Autowired
 	TestRestTemplate template;
+
+	@BeforeClass
+	public static void init() {
+		System.setProperty("spring.cloud.gateway.routes[0].id", "account-service");
+		System.setProperty("spring.cloud.gateway.routes[0].uri", "http://192.168.99.100:" + mockServer.getServerPort());
+		System.setProperty("spring.cloud.gateway.routes[0].predicates[0]", "Path=/account/**");
+		System.setProperty("spring.cloud.gateway.routes[0].filters[0]", "RewritePath=/account/(?<path>.*), /$\\{path}");
+		new MockServerClient(mockServer.getContainerIpAddress(), mockServer.getServerPort())
+				.when(HttpRequest.request()
+						.withPath("/1"))
+				.respond(response()
+						.withBody("{\"id\":1,\"number\":\"1234567890\"}")
+						.withHeader("Content-Type", "application/json"));
+	}
 
 	@Test
 	public void testAccountService() {
