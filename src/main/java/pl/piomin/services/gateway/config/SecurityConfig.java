44eb9a832d6ae267736b16c5ac2c77a5b1798d92
piomin <piotr.minkowski@gmail.com>
2021-05-18 16:06:52 +0200

    Adding security #2

--- src/main/java/pl/piomin/services/gateway/config/SecurityConfig.java
+++ src/main/java/pl/piomin/services/gateway/config/SecurityConfig.java
@@ -3,18 +3,15 @@ package pl.piomin.services.gateway.config;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-//@ConditionalOnProperty("rateLimiter.secure")
+@ConditionalOnProperty("rateLimiter.secure")
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
@@ -24,15 +21,6 @@ public class SecurityConfig {
                 .httpBasic();
         http.csrf().disable();
         return http.build();
-//        return http
-//                .csrf().disable()
-//                .authorizeExchange()
-//                .anyExchange().authenticated()
-//                .and()
-//                .httpBasic()
-//                .and()
-//                .formLogin().disable()
-//                .build();
     }
 
     @Bean
